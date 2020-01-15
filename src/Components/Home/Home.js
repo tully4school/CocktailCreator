fedc215fcfdee1d4678a71ed5d349569422a10a6
Joseph_Tully <tully4school@gmail.com>
2020-01-15 06:41:14 -0500

    added homepage header

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -15,6 +15,11 @@ class Home extends Component {
         return (
             <>
                 <div className='row'>
+                    <div className='col-lg-12'>
+                        <h1 className='crud-create-header display-4 mt-5 text-center'>Cocktail Creator</h1>
+                    </div>
+                </div>
+                <div className='row'>
                     <div className='d-flex col-12 d-lg-none mt-5 justify-content-center'>
                         <SearchBar />
                     </div>
