748439cac1271add1dd6f0581e4d572d3870a57f
Joseph_Tully <tully4school@gmail.com>
2020-01-15 06:21:08 -0500

    testing

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,9 +5,30 @@ class About extends Component {
 		return (
 			<div className='container'>
 				<div className='row'>
-					<div className='col-md-12'>
-						<h1 className='about'>About Cocktail Creator</h1>
+					<div className='col-lg-12'>
+						<h1 className='crud-create-header display-4 mt-5 text-center'>About Cocktail Creator</h1>
 					</div>
+					<div className="col-lg-12">
+						<h4 className="text-center mb-5">School Project 6</h4>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-lg-12">
+						<p><blockquote className="blockquote">"You will be building a JSON API using Mongoose and Express. In addition, you will find a dataset online to pull data from. You'll use this retrieved data to build your own model(s), and also to seed your database with. Requirements: Be built using Express and Mongoose, Have at least one model,Have complete CR functionality implemented with RESTful routes, Be deployed to Heroku, Have well-documented API routes."</blockquote> This was pretty fun, but i'm not gonna lie, it was a bit difficult thinking of something i wanted to make an API about. BUT i settled with this and it was a fun experience especially knowing that later that week we would start the build of a front-end. The documentation was completed via SwaggerDocs which a classmate and I had a blast learning about.</p>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-lg-12 text-center"><p>My name is Joseph Tully, I'm a software engineer. Thanks for stopping by.</p></div>
+				</div>
+				<div className="row">
+					<div className="col-3 col-lg-1 offset-lg-2 my-3 d-flex align-items-center"><img className="img-fluid" src="https://relaxed-curran-4b9888.netlify.com/static/media/HTML5.b869b6bc.png" alt="" /></div>
+					<div className="col-3 col-lg-1 my-3 d-flex align-items-center"><img className="img-fluid" src="https://relaxed-curran-4b9888.netlify.com/static/media/css.c26a73b4.png" alt="" /></div>
+					<div className="col-3 col-lg-1 my-3 d-flex align-items-center"><img className="img-fluid" src="https://relaxed-curran-4b9888.netlify.com/static/media/js.5b22ce57.png" alt="" /></div>
+					<div className="col-3 col-lg-1 my-3 d-flex align-items-center"><img className="img-fluid" src="https://relaxed-curran-4b9888.netlify.com/static/media/node.2a694fea.png" alt="" /></div>
+					<div className="col-3 col-lg-1 my-3 d-flex align-items-center"><img className="img-fluid" src="https://relaxed-curran-4b9888.netlify.com/static/media/mongodb.b5f7d0bf.png" alt="" /></div>
+					<div className="col-3 col-lg-1 my-3 d-flex align-items-center"><img className="img-fluid" src="https://relaxed-curran-4b9888.netlify.com/static/media/React.edb8f280.png" alt="" /></div>
+					<div className="col-3 col-lg-1 my-3 d-flex align-items-center"><img className="img-fluid" src="https://relaxed-curran-4b9888.netlify.com/static/media/heroku.40b07e60.png" alt="" /></div>
+					<div className="col-3 col-lg-1 my-3 d-flex align-items-center"><img className="img-fluid" src="https://relaxed-curran-4b9888.netlify.com/static/media/swagger.83929eaa.png" alt="" /></div>
 				</div>
 			</div>
 		);
--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,15 +1,109 @@
 import React, { Component } from "react";
-class Contact extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
+import * as emailjs from "emailjs-com";
+import {
+	Button,
+	FormFeedback,
+	Form,
+	FormGroup,
+	Label,
+	Input
+} from "reactstrap";
+class ContactForm extends Component {
+	state = {
+		name: "",
+		email: "",
+		subject: "",
+		message: ""
+	};
+	handleSubmit(e) {
+		e.preventDefault();
+		const { name, email, subject, message } = this.state;
+		let templateParams = {
+			from_name: email,
+			to_name: "gmail",
+			subject: subject,
+			message_html: message
+		};
+		emailjs.send(
+			"gmail",
+			"template_dYZBD6YN",
+			templateParams,
+			"user_nKtggWtjaykr9NHaBaTuF"
+		);
+		this.resetForm();
 	}
+	resetForm() {
+		this.setState({
+			name: "",
+			email: "",
+			subject: "",
+			message: ""
+		});
+	}
+	handleChange = (param, e) => {
+		this.setState({ [param]: e.target.value });
+	};
 	render() {
 		return (
-			<div>
-				<div className='Contact'></div>
-			</div>
+			<>
+				<div className='row'>
+					<div className='col-md-12'>
+						<h1 className='crud-create-header display-4 mt-5 text-center'>Reach Out</h1>
+					</div>
+				</div>
+				<Form
+					onSubmit={this.handleSubmit.bind(this)}
+					className='m-2 m-md-5 px-md-5'
+				>
+					<FormGroup controlId='formBasicEmail'>
+						<Label className='text-muted'>Email address</Label>
+						<Input
+							type='email'
+							name='email'
+							value={this.state.email}
+							className='text-primary'
+							onChange={this.handleChange.bind(this, "email")}
+							placeholder='Apriloneil@emailaddress.com'
+						/>
+					</FormGroup>
+					<FormGroup controlId='formBasicName'>
+						<Label className='text-muted'>Name</Label>
+						<Input
+							type='text'
+							name='name'
+							value={this.state.name}
+							className='text-primary'
+							onChange={this.handleChange.bind(this, "name")}
+							placeholder="April O'Neil"
+						/>
+					</FormGroup>
+					<FormGroup controlId='formBasicSubject'>
+						<Label className='text-muted'>Subject</Label>
+						<Input
+							type='text'
+							name='subject'
+							className='text-primary'
+							value={this.state.subject}
+							onChange={this.handleChange.bind(this, "subject")}
+							placeholder='Reason For Message'
+						/>
+					</FormGroup>
+					<FormGroup controlId='formBasicMessage'>
+						<Label className='text-muted'>Message</Label>
+						<Input
+							type='textarea'
+							name='message'
+							className='text-primary'
+							value={this.state.message}
+							onChange={this.handleChange.bind(this, "message")}
+						/>
+					</FormGroup>
+					<Button variant='primary' type='submit'>
+						Submit
+					</Button>
+				</Form>
+			</>
 		);
 	}
 }
-export default Contact;
+export default ContactForm;
